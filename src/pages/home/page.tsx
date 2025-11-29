4659782706cc83714e1938846079995e8d72b7a9
Migueltri <mitrijurrea@gmail.com>
2025-11-29 10:20:00 +0100

    Update page.tsx

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -492,9 +492,9 @@ const HomePage = () => {
             ))}
           </div>
         </div>
-      </section>
-
-          {/* Trust Badges */}
+           {/* Trust Badges */}
+      <section className="py-16">
+        <div className="max-w-7xl mx-auto px-6">
           <div className="mt-16 flex flex-wrap justify-center items-center gap-8">
             <div className="flex items-center gap-3 text-gray-600">
               <i className="ri-shield-check-fill text-3xl text-green-600"></i>
@@ -519,6 +519,8 @@ const HomePage = () => {
             </div>
           </div>
         </div>
+      </section>
+
 
 
       {/* Newsletter Section */}
